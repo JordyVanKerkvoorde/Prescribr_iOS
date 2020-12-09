efc495b9a6c138c948698186faa4e2cc3183cffe
Jordy <jordy1@live.be>
2020-12-09 17:47:24 +0100

    Add logout

---
+++ Prescribr/Controllers/SettingsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsViewController.swift
+//  Prescribr
+//
+//  Created by Jordy Van Kerkvoorde on 09/12/2020.
+//
+
+import Foundation
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    
+    @IBAction func onLogout(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.removeObject(forKey: "TOKEN")
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let loginController = storyboard.instantiateViewController(identifier: "login")
+        
+        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(loginController)
+    }
+    
+}
