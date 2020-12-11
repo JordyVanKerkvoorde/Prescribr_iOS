f062e625a577a5bb3ecdd6b9fe4ffe92a23bbf25
Jordy <jordy1@live.be>
2020-12-11 17:04:58 +0100

    Add tableview and working segue

---
+++ Prescribr/Controllers/PatientsTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PatientsTableViewController.swift
+//  Prescribr
+//
+//  Created by Jordy Van Kerkvoorde on 11/12/2020.
+//
+
+import UIKit
+
+class PatientsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
