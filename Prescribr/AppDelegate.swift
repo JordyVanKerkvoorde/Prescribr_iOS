8a00279c8ddfea730e46a4fa898bd8db400c417d
Jordy <jordy1@live.be>
2020-12-30 00:02:53 +0100

    Refactor patient page + add styling to drug pages.

--- Prescribr/AppDelegate.swift
+++ Prescribr/AppDelegate.swift
@@ -15,9 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         UITabBar.appearance().barTintColor = .systemGray2
-        UITabBar.appearance().tintColor = .cyan
+        UITabBar.appearance().tintColor = .systemIndigo
         UITabBar.appearance().unselectedItemTintColor = .darkGray
-
         
         return true
     }
--- Prescribr/Controllers/PatientsTableViewController.swift
+++ Prescribr/Controllers/PatientsTableViewController.swift
@@ -7,16 +7,24 @@
 
 import UIKit
 
-class PatientsTableViewController: UITableViewController {
+class PatientsTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     public var patients: [Patient] = []
+    var filteredPatients: [Patient] = []
+    
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var searchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        searchBar.delegate = self
         
         PatientService().getPatients(){ (response, fail) in
             if response != nil {
                 self.patients = response!
+                self.filteredPatients = self.patients
             }
             if fail != nil {
                 print("REQUEST FAILED")
@@ -27,19 +35,20 @@ class PatientsTableViewController: UITableViewController {
         }
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return patients.count
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredPatients.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PatientCell", for: indexPath) as! PatientCell
-        let patient = patients[indexPath.item]
+        let patient = filteredPatients[indexPath.item]
         cell.setName(firstName: patient.firstName, lastName: patient.lastName)
         
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "ShowPatientsDetails", sender: indexPath.row)
     }
     
@@ -48,10 +57,26 @@ class PatientsTableViewController: UITableViewController {
             let detailViewController = segue.destination as! PatientsDetailViewController
             
             let index = (self.tableView.indexPathForSelectedRow?.item)!
-            let patient = patients[index]
+            let patient = filteredPatients[index]
             
             detailViewController.patient = patient
         }
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredPatients = []
+        if searchText == "" {
+            filteredPatients = patients
+        }else {
+            for patient in patients{
+                let name = "\(patient.firstName) \(patient.lastName)"
+                if name.lowercased().contains(searchText.lowercased()) {
+                    filteredPatients.append(patient)
+                }
+            }
+        }
+        
+        self.tableView.reloadData()
     }
 
 }
